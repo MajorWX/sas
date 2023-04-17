b21f437a1618ccdf23f69fdf3834f398db999657
Kira Beitler <majorwx2@gmail.com>
2023-04-17 19:01:18 -0400

    Completes the code for finding a single record.

--- phase04/private/query_functions.php
+++ phase04/private/query_functions.php
@@ -1,13 +1,26 @@
 <?php
 
-  function find_all_subjects() {
-    global $db;
+function find_all_subjects()
+{
+  global $db;
 
-    $sql = "SELECT * FROM salamander ";
-    $sql .= "ORDER BY name ASC";
-    $result = mysqli_query($db, $sql);
-    confirm_result_set($result);
-    return $result;
-  }
+  $sql = "SELECT * FROM salamander ";
+  $sql .= "ORDER BY name ASC";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+  return $result;
+}
 
-?>
+function find_subject_by_id($id)
+{
+  global $db;
+
+  $sql = "SELECT * FROM salamander ";
+  $sql .= "WHERE id='" . $id . "'";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+
+  $salamander = mysqli_fetch_assoc($result);
+  mysqli_free_result($result);
+  return $salamander; //Returns an assoc array
+}
