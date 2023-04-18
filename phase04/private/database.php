f94c1787ef05d54ebb0acc27d7671a596ab1ef40
Kira Beitler <majorwx2@gmail.com>
2023-04-17 21:05:20 -0400

    Added db_credentials.php and a README.

--- phase04/private/database.php
+++ phase04/private/database.php
@@ -2,19 +2,9 @@
 
   function db_connect() {
 
-    if($_SERVER['SERVER_NAME'] == 'majorwx.xyz') {
-      // Siteground Credentials
-      $dbhost = 'localhost';
-      $dbuser = 'ufhniitg74txz';
-      $dbpass = '~tnE17#g2#4z';
-      $dbname = 'dbhfirzwlacnum';
-    } else {
-      // Localhost Credentials
-      $dbhost = 'localhost';
-      $dbuser = 'sally';
-      $dbpass = 'P@ssword1234';
-      $dbname = 'salamanders';
-    }
+    require_once('db_credentials.php');
+
+    
 
     $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
     confirm_db_connect();
--- phase04/private/db_connect.php
+++ phase04/private/db_connect.php
@@ -4,19 +4,8 @@
 // of database interaction using PHP.
 
 
-if($_SERVER['SERVER_NAME'] == 'majorwx.xyz') {
-  // Siteground Credentials
-  $dbhost = 'localhost';
-  $dbuser = 'ufhniitg74txz';
-  $dbpass = '~tnE17#g2#4z';
-  $dbname = 'dbphwiqkyidx4a';
-} else {
-  // Localhost Credentials
-  $dbhost = 'localhost';
-  $dbuser = 'sally';
-  $dbpass = 'P@ssword1234';
-  $dbname = 'salamanders';
-}
+
+require_once('db_credentials.php');
 
 $connection == mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 mysqli_close($connection);
